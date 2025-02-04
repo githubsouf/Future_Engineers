cf67a373d2308ecd6f147eb785ad0ca94d097ffb
Ayoubouladmaalem <ouladmaalme.ayoub@etu.uae.ac.ma>
2025-02-04 11:22:20 +0100

    pulling api

--- API Backend/src/main/java/org/example/futureengineers/Config/SecurityConfig.java
+++ API Backend/src/main/java/org/example/futureengineers/Config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5175")); // Your frontend's origin
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Your frontend's origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Allowed headers
         configuration.setAllowCredentials(true); // Allow credentials (e.g., cookies, Authorization header)
